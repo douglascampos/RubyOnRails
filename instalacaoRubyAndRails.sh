c918d9fbf3ad1101f5206360702245449c8756c2
Eric Dallo <ericdallo06@hotmail.com>
2015-06-18 00:58:41 -0300

    Primeiro commit, aplicação básica rails

---
+++ instalacaoRubyAndRails.sh
@@ -0,0 +1,3 @@
+sudo apt-get install build-essential libreadline-dev libssl-dev curl libsqlite3-dev
+
+bash < <(curl -sL https://raw.github.com/railsgirls/installation-scripts/master/rails-install-ubuntu.sh)
